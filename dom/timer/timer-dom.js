d171749e50daf32819f40c08e8a49b3a0a0eb1c1
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2021-01-29 01:34:10 -0600

    finished timer problem

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,2 +1,44 @@
-console.log("time to start the timer!");
+let time = document.querySelector('p')
+
+let startButton = document.querySelector('button')
+let stopButton = document.querySelector('.btn2')
+let resetButton = document.querySelector('.btn3') 
+
+let timerId;
+let counter = 10;
+
+time.innerHTML = counter
+
+function start() {
+    if (!counter) {
+        reset();
+    } else {
+        clock();
+    }
+}
+  
+function stop() {
+    if (timerId) {
+        clearInterval(timerId);
+        timerId = null;
+    }
+}
+  
+function reset() {
+    stop();
+    counter = 10;
+    time.innerHTML = counter
+}
+  
+function clock() {
+    timerId = setInterval(function() {
+        if (-1 >= counter) {
+            stop();
+            return;
+        }
+        counter--;
+        time.innerHTML = counter
+    }, 700);
+}
+ 
 
